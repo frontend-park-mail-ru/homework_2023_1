9d1d3c62ef832fb6d81441fe2187378d138dc982
dyndtikj <vlamaster08@gmail.com>
2023-02-08 23:07:22 +0300

    Edit doc

--- source/letters.js
+++ source/letters.js
@@ -9,17 +9,14 @@
  * if flag == undefined - deletes All instances of repeating letter
  * @returns {string} Returns string without repeating letters (depends on flag)
  * @example
- * // returns "a_s1"
- * letters("a_aas1s", true);
+ * // returns "a_b1"
+ * letters("a_aas1b", true);
  * @example
- * // returns "_a1s"
- * letters("a_aass", false);
- * @example
- * // returns "_a1s"
- * letters("a_aass", false);
+ * // returns "_a1b"
+ * letters("a_aabb", false);
  * @example
  * // returns "_"
- * letters("a_aass");
+ * letters("a_aabb");
  */
 
 function letters (str, flag) {
