bfdb515dae4f5906a2c551eb1e1fda82535c715a
dyndtikj <vlamaster08@gmail.com>
2023-02-08 22:52:51 +0300

    Add doc

--- source/letters.js
+++ source/letters.js
@@ -1,7 +1,29 @@
 "use strict";
 
-function letters (string, flag) {
-    return string.split('').filter((element, idx, array) => {
+/**
+ * @param {string} str - string for deleting repeating letters
+ * @param {boolean} flag
+ * @description deletes repeating symbols from string
+ * if flag == true  -  keeps only First instance of repeating letter
+ * if flag == false -  keeps only Last instance of repeating letter
+ * if flag == undefined - deletes All instances of repeating letter
+ * @returns {string} Returns string without repeating letters (depends on flag)
+ * @example
+ * // returns "a_s1"
+ * letters("a_aas1s", true);
+ * @example
+ * // returns "_a1s"
+ * letters("a_aass", false);
+ * @example
+ * // returns "_a1s"
+ * letters("a_aass", false);
+ * @example
+ * // returns "_"
+ * letters("a_aass");
+ */
+
+function letters (str, flag) {
+    return str.split('').filter((element, idx, array) => {
         if (flag === undefined) {
             return array.lastIndexOf(element) === array.indexOf(element)
         } else return  (flag) ? array.indexOf(element) === idx
