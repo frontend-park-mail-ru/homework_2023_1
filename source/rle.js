bf2e3abbdce74eb21b5b69c4e934aac53d61c5a7
moraPresence <od34156la@mail.ru>
2023-02-09 12:15:04 +0300

    JSDocs added

--- source/rle.js
+++ source/rle.js
@@ -1,8 +1,15 @@
 'use strict';
 
+/**
+ * Функция RLE сжатия
+ * @param str любая строка, например ABBB
+ * @returns {string|null} сжатая строка с помощью RLE, например AB3
+ */
 const rle = function (str) {
-  if (str == null || typeof str != 'string'){
+  if (str == null || typeof str != 'string') {
     return null;
   }
-  return str.replace(/(.)\1+/g, function (internalStr, symbol) { return symbol + internalStr.length; });
+  return str.replace(/(.)\1+/g, function (internalStr, symbol) {
+    return symbol + internalStr.length;
+  });
 };
