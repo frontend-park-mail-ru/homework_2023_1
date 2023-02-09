df18f9ee64d7b49624858447afee964e48e6a9f5
Fedor <bazaleevf@gmail.com>
2023-02-09 13:01:17 +0300

    add JsDoc

--- source/chess.js
+++ source/chess.js
@@ -1,12 +1,16 @@
 'use strict';
 
+/** The function returns an even checkerboard row of stars and spaces. */
 const getEvenLine = numbers => "* ".repeat(numbers / 2) + "*".repeat(numbers % 2) + "\n";
 
+/** The function returns an odd checkerboard row of stars and spaces. */
 const getOddLine = numbers => " *".repeat(numbers / 2) + " ".repeat(numbers % 2) + "\n";
 
+/** A function that returns a chessboard N*N of stars and spaces. */
 const chess = (numbers) => {
-    if (!Number.isInteger(+numbers) || numbers < 2) return null;
+    if ( !Number.isInteger(+numbers) || numbers < 2) {
+        return null;
+    }
 
     return (getEvenLine(numbers) + getOddLine(numbers)).repeat(numbers / 2) + getEvenLine(numbers).repeat(numbers % 2);
 };
-
