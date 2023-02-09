32ebbbfb5311d50cd0a2e14928e1f66843eda510
stepanpopov <stepan@DESKTOP-81FUGHF>
2023-02-09 16:06:10 +0300

    filter done

---
+++ source/filter.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let extra = new Map([['>', "&gt;"],
+                    ['<', "&lt;"],
+                    ['"', "&quot;"],
+                    ['&', "&amp;"],
+                    ['\'', "&#39;"]]);
+                    
+let filter = function(input, tags) {
+    let output = ""
+    for (let i = 0; i < input.length; ++i) {
+        let changedSymb = extra.get(input[i])
+        if (changedSymb === undefined) {
+            output += input[i]
+        } else {
+            output += changedSymb
+        }
+    }
+    
+    if (tags === undefined) {
+        return output
+    }
+    
+    for (let tag of tags) {
+        let tag_len = tag.length;
+        let gt_sym_len = extra.get('>').length;
+        let lt_sym_len = extra.get('<').length; 
+        
+        let pos = -1;
+        while ((pos = output.indexOf(tag, pos + 1)) != -1) {
+            if (output[pos - 1] == '/') {
+                output = output.slice(0, pos - lt_sym_len - 1) + 
+                         "</" + tag + ">" + 
+                         output.slice(pos + tag_len + gt_sym_len);
+            } else {
+                output = output.slice(0, pos - lt_sym_len) + 
+                         "<" + tag + ">" + 
+                         output.slice(pos + tag_len + gt_sym_len);
+            }
+        }
+    }
+    return output
+};
--- tests/filter.js
+++ tests/filter.js
@@ -25,4 +25,14 @@ QUnit.module('Проверка работы функции filter', function () 
 		assert.strictEqual(filter(`<script>alert('1');</script>`, [ 'strong', 'em' ]), '&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;');
 		assert.strictEqual(filter(`<img src="bad" onerror="alert('1');">`, [ 'strong', 'em' ]), '&lt;img src=&quot;bad&quot; onerror=&quot;alert(&#39;1&#39;);&quot;&gt;');
 	});
+
+	/* QUnit.test('filter экранирует XSS', function (assert) {
+		const input = "<ul class='navbar-nav me-auto mb-2 mb-md-0'> <li class='nav-item'> <a class='nav-link' aria-current='page' href={% url 'index' %}>New</a> </li> </ul>"
+
+		const output = filter(input,)
+
+		const expected = 
+
+		assert.strictEqual(filter(`<script>alert('1');</script>`, [ 'strong', 'em' ]), '&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;');
+		});*/
 });
