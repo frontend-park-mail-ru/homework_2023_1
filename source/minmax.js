75e619de47f293717bcce6b13fd590ec90660100
Aleksandr <sasha.oparin.1984@mail.ru>
2023-02-09 12:57:40 +0300

    variant-12

---
+++ source/minmax.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const min = numbers => Math.min(...numbers);
+const max = numbers => Math.max(...numbers);
+
+const minmax = (str) => {
+    let numbers = str.split(' ').filter(Boolean).map(Number).filter((value) => !Number.isNaN(value))
+    return numbers.length ? [min(numbers), max(numbers)] : [undefined, undefined]
+};
