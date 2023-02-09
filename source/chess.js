66e73481cb20b6e59be2796cb628fcff7ea98dce
Bazaleev Fedor <bazaleevf@gmail.com>
2023-02-09 15:43:31 +0300

    change quotes

--- source/chess.js
+++ source/chess.js
@@ -6,10 +6,10 @@ const STAR = '*';
 const SPACE = ' ';
 
 /** The function returns an even checkerboard row of stars and spaces. */
-const getEvenLine = numbers => EVEN_LINE_PART.repeat(numbers / 2) + STAR.repeat(numbers % 2) + '\n';
+const getEvenLine = (numbers) => EVEN_LINE_PART.repeat(numbers / 2) + STAR.repeat(numbers % 2) + '\n';
 
 /** The function returns an odd checkerboard row of stars and spaces. */
-const getOddLine = numbers => ODD_LINE_PART.repeat(numbers / 2) + SPACE.repeat(numbers % 2) + '\n';
+const getOddLine = (numbers) => ODD_LINE_PART.repeat(numbers / 2) + SPACE.repeat(numbers % 2) + '\n';
 
 /** A function that returns a chessboard N*N of stars and spaces. */
 const chess = (numbers) => {
