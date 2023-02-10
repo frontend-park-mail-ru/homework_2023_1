782d3131b3d2d4a92491f7a90c7543a3fbb4a2bb
niki-gor <nic.gor@yandex.ru>
2023-02-10 22:32:35 +0300

    Make all attrs like ['attr']

--- source/max.js
+++ source/max.js
@@ -5,16 +5,15 @@ const max = numbers => Math.max(...numbers);
 /**
  * Apply string of attributes to object
  * @param {Object} obj - object
- * @param {string} attrs - string of attributes of the form '.attr1.attr2.attrN'. Any attr can be numeric
- * @returns {Object} - object that can be interpreted as obj.attr1.attr2.attrN
+ * @param {string} attrs - string of attributes of the form '.attr1.attr2.attrN'. Any attr can be numeric too
+ * @returns {Object} - object that can be interpreted as obj['attr1']['attr2']['attrN']
  */
 function get(obj, attrs) {
     if (attrs == '.') {
         return obj
     }
     try {
-        // numbers must be in [] since they can be used as array indices
-        return eval('obj' + attrs.replaceAll(/\.(\d+)/g, '[$1]'))
+        return eval('obj' + attrs.replaceAll(/\.(\w+)/g, "['$1']"))
     } catch (e) {
         if (e.name != 'TypeError') {
             throw e
