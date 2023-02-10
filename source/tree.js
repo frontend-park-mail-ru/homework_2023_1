7006ca248313af876c5cbe8eca85bb95f43e712f
Azat-Bilalov <52991059+Azat-Bilalov@users.noreply.github.com>
2023-02-10 20:31:09 +0300

    Problems fixed

--- source/tree.js
+++ source/tree.js
@@ -1,28 +1,42 @@
 'use strict';
 
+const SPACE = ' ';
+const CROWN = '*';
+const TRUNK = '|';
+
+/**
+ * Функция, которая добавляет слой к дереву.
+ * @param {String} element - Элемент слоя.
+ * @param {Integer} n - Номер слоя.
+ * @param {Integer} width - Максимальная ширина дерева.
+ * @returns {String} Слой дерева.
+ */
+const addLayer = (element, n, width) => {
+    let layer = '';
+    layer += SPACE.repeat(width / 2 - n);
+    layer += element.repeat(n * 2 + 1);
+    layer += SPACE.repeat(width / 2 - n);
+    layer += '\n';
+    return layer;
+}
+
+/**
+ * Функция, которая рисует дерево.
+ * @param {Integer} height - Высота дерева.
+ * @returns {String} Дерево.
+ */
 const tree = (height) => {
-    if (height === undefined || height < 3) {
+    if (!Number.isInteger(+height) || height < 3) {
         return null;
     }
 
-    const space = ' ';
-    const crown = '*';
-    const trunk = '|';
     let tree = '';
-    let max_width = (height - 3) * 2 + 2;
-
-    const addLayer = (element, n) => {
-        tree += space.repeat(max_width / 2 - n);
-        tree += element.repeat(n * 2 + 1);
-        tree += space.repeat(max_width / 2 - n);
-        tree += '\n';
-        return tree;
-    }
+    const width = (height - 3) * 2 + 2;
 
     for (let i = 0; i < height - 1; i++) {
-        addLayer(crown, i);
+        tree += addLayer(CROWN, i, width);
     }
-    addLayer(trunk, 0);
+    tree += addLayer(TRUNK, 0, width);
 
     return tree;
 }
--- tests/tree.js
+++ tests/tree.js
@@ -1,8 +1,41 @@
 'use strict';
 
+QUnit.module('Тестируем функцию addLayer', function () {
+	QUnit.test('Слои дерева шириной 3', function (assert) {
+		assert.strictEqual(addLayer('*', 0, 3), ' * \n');
+		assert.strictEqual(addLayer('*', 1, 3), '***\n');
+		assert.strictEqual(addLayer('|', 0, 3), ' | \n');
+	});
+
+	QUnit.test('Слои дерева шириной 5', function (assert) {
+		assert.strictEqual(addLayer('*', 0, 5), '  *  \n');
+		assert.strictEqual(addLayer('*', 1, 5), ' *** \n');
+		assert.strictEqual(addLayer('*', 2, 5), '*****\n');
+		assert.strictEqual(addLayer('|', 0, 5), '  |  \n');
+	});
+
+	QUnit.test('Слои дерева шириной 999', function (assert) {
+		assert.strictEqual(addLayer('*', 0, 999).length, 1000);
+		assert.strictEqual(addLayer('*', 1, 999).length, 1000);
+		assert.strictEqual(addLayer('*', 2, 999).length, 1000);
+		assert.strictEqual(addLayer('|', 0, 999).length, 1000);
+		assert.strictEqual(addLayer('*', 0, 999).trim(), '*');
+		assert.strictEqual(addLayer('*', 1, 999).trim(), '***');
+		assert.strictEqual(addLayer('*', 500, 999), '*'.repeat(1001) + '\n');
+
+		const trunk_expected = ' '.repeat(500) + '|' + ' '.repeat(500) + '\n';
+		assert.strictEqual(addLayer('|', 0, 1001), trunk_expected); 
+	});
+});
+
 QUnit.module('Тестируем функцию tree', function () {
-	QUnit.test('Ёлочек неопределенной высоты не бывает', function (assert) {
+	QUnit.test('Ёлочек нечисловой высоты не бывает', function (assert) {
 		assert.strictEqual(tree(undefined), null);
+		assert.strictEqual(tree(), null);
+		assert.strictEqual(tree(null), null);
+		assert.strictEqual(tree({}), null);
+		assert.strictEqual(tree([]), null);
+		assert.strictEqual(tree(''), null);
 	});
 
 	QUnit.test('Ёлочек высотой ниже трёх не бывает', function (assert) {
@@ -97,5 +130,20 @@ QUnit.module('Тестируем функцию tree', function () {
 			'        |        \n';
 		assert.strictEqual(tree(10), expected);
 		assert.strictEqual(tree('10'), expected);
+	});
+
+	QUnit.test('Ёлочка высотой 100', function (assert) {
+		const expected_1 = ' '.repeat(98) + '*' + ' '.repeat(98);
+		const expected_2 = ' '.repeat(97) + '***' + ' '.repeat(97);
+		const expected_99 = '*'.repeat(197);
+		const expected_100 = ' '.repeat(98) + '|' + ' '.repeat(98);
+		const generated = tree(100);
+		const layers = generated.split('\n');
+
+		assert.strictEqual(layers.length, 101);
+		assert.strictEqual(layers[0], expected_1);
+		assert.strictEqual(layers[1], expected_2);
+		assert.strictEqual(layers[98], expected_99);
+		assert.strictEqual(layers[99], expected_100);
 	});
 });
