c03cd237c4362ccc224fbe2d7a9a2ffac1d9679d
niki-gor <nic.gor@yandex.ru>
2023-02-10 19:30:53 +0300

    Implement get

--- source/max.js
+++ source/max.js
@@ -1,3 +1,18 @@
 'use strict';
 
 const max = numbers => Math.max(...numbers);
+
+function get(obj, attr) {
+    if (attr == '.') {
+        return obj
+    }
+    try {
+        // numbers must be in [] since they can be used as array indices
+        return eval('obj' + attr.replaceAll(/\.(\d+)/g, '[$1]'))
+    } catch (e) {
+        if (e.name != 'TypeError') {
+            throw e
+        }
+    }
+    return undefined
+}
