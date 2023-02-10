c96fcbe562789afb3c4fa7483636557a0640fb4b
Kirill Kiselev <lirikk285@mail.ru>
2023-02-10 10:53:47 +0300

    Исправил условие для проверки переданных аргументов. Добавил новые тесты

--- source/zip.js
+++ source/zip.js
@@ -1,14 +1,13 @@
 'use strict';
 
 /**
- * 
- * @param {Object[]} objects - objects array for zip.
+ *accepts objects (any number) as input, and return a single object containing all fields from all objects.
+ * @param {Object[]} objects - Objects for zip.
  * @returns {Object}
  */
-
 const zip = (...objects) => {
   return objects.reduceRight((result, obj, index) => {
-    if ((obj === null) || (typeof obj !== 'object')) throw new Error(`Argument №${index} is not object`)
+    if (Object.prototype.toString.call(obj) !== "[object Object]") throw new Error(`Argument №${index} is not object`)
     Object.keys(obj).forEach((key) => {result[key] = obj[key]}) 
     return result
   }, {})
