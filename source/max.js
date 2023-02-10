c2444e217ba35cf23d69806d2e10e49c575c0fe0
niki-gor <nic.gor@yandex.ru>
2023-02-10 22:26:19 +0300

    Add docs

--- source/max.js
+++ source/max.js
@@ -2,13 +2,19 @@
 
 const max = numbers => Math.max(...numbers);
 
-function get(obj, attr) {
-    if (attr == '.') {
+/**
+ * Apply string of attributes to object
+ * @param {Object} obj - object
+ * @param {string} attrs - string of attributes of the form '.attr1.attr2.attrN'. Any attr can be numeric
+ * @returns {Object} - object that can be interpreted as obj.attr1.attr2.attrN
+ */
+function get(obj, attrs) {
+    if (attrs == '.') {
         return obj
     }
     try {
         // numbers must be in [] since they can be used as array indices
-        return eval('obj' + attr.replaceAll(/\.(\d+)/g, '[$1]'))
+        return eval('obj' + attrs.replaceAll(/\.(\d+)/g, '[$1]'))
     } catch (e) {
         if (e.name != 'TypeError') {
             throw e
