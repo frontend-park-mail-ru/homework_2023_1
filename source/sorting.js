ea1386cb3c7e80139c5023fdeb3b2d71a2d2417d
IvanStukalov <ivanstukalov2003@gmail.com>
2023-02-10 14:33:19 +0300

    jsdoc

--- source/sorting.js
+++ source/sorting.js
@@ -1,11 +1,21 @@
 'use strict';
 
+/**
+ * Sorting an array
+ * @sorting
+ * @param {Array} arr - Array you want to sort
+ * @param {Array} params - array of parameters you will sort on
+ * 
+ * @return {Array} Array that have been sorted
+ * 
+ * @author Ivan Stuaklov <sid21u699@student.bmstu.ru>
+ */
 const sorting = function (arr, params) {
     for (let curParam = 0; curParam < params.length; ++curParam) {
         arr.sort((a, b) => {
             let wasCoincident = true;
             for (let lastParam = 0; lastParam < curParam; ++lastParam){ // проверяем, равны ли значения по всем предыдущим параметрам
-                if (a[params[lastParam]] != b[params[lastParam]]) {     // (были ли совпадения)
+                if (a[params[lastParam]] != b[params[lastParam]]) {     
                     wasCoincident = false;
                 }
             }
