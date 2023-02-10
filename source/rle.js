ab84205303cf173fc1b80cfd839f12292632c42e
Alik Nigmatullin <NigAlik020503@yandex.ru>
2023-02-10 23:43:38 +0300

    added some tests and jsdoc

--- source/rle.js
+++ source/rle.js
@@ -1,21 +1,29 @@
 //First version
 // 'use strict';
 // const rle = function (str) {
+//     if (str === '') return '';
 //     let res = '';
 //     let count = 1;
 //     for (let i = 1; i < str.length; ++i) {
-//         if (str[i] === str[i-1]) {
-//             count++;
-//         }
-//         else {
-//             (count !== 1) ? res += str[i-1] + count : res += str[i-1]
-//             count = 1;
-//         }
+//         (str[i] === str[i-1]) ?
+//             count++ :
+//             ((count !== 1) ?
+//                 res += str[i-1] + count :
+//                 res += str[i-1],
+//                 count = 1)
 //     }
-//     return (count !== 1) ? res + str[str.length-1] + count : res + str[str.length-1]
+//     return (count !== 1) ? res + str.at(-1) + count : res + str.at(-1)
 // }
 
 // Last version
 'use strict';
+
+/**
+ * RLE compression
+ * @param {string} str - string you want to compress
+ *
+ * @returns {string} - compressed string
+ * @author Nigmatullin Alik <NigAlik020503@yandex.ru>
+ */
 const rle = str => str.replace(/(.)\1*/g,
     function (substr, sym) { return sym + (substr.length > 1 ? substr.length : ''); });
--- tests/rle.js
+++ tests/rle.js
@@ -1,12 +1,25 @@
 'use strict';
 
 QUnit.module('Тестируем функцию rle', function () {
-	QUnit.test('rle работает правильно', function (assert) {
-		assert.strictEqual(rle('AAAB'), 'A3B');
-		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
-		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
-		assert.strictEqual(rle(''), '');
-		assert.strictEqual(rle('AAAAAAAAAA'), 'A10');
-		assert.strictEqual(rle('A'), 'A');
+	QUnit.test('Функция работает с пустой строкой', function (assert) {
+		assert.strictEqual(rle(''), '', "rle('') === ''");
+	});
+	QUnit.test('Функция работает со строкой из одного символа', function (assert) {
+		assert.strictEqual(rle('A'), 'A', "rle('A') === 'A'");
+	});
+	QUnit.test('Функция работает со строкой из повторяющихся символов', function (assert) {
+		assert.strictEqual(rle('AAAAAAAAAA'), 'A10', "rle('AAAAAAAAAA') === 'A10'");
+	});
+	QUnit.test('Функция работает со строкой, в конце которой один последний символ', function (assert) {
+		assert.strictEqual(rle('AAAB'), 'A3B', "rle('AAAB') === 'A3B'");
+	});
+	QUnit.test('Функция работает со строкой, в конце которой повторяющиеся символы', function (assert) {
+		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4', "rle('BCCDDDAXXXX') === 'BC2D3AX4'");
+	});
+	QUnit.test('Функция работает с длинной строкой', function (assert) {
+		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4',
+			"rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD') === 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4'");
 	});
 });
+
+
