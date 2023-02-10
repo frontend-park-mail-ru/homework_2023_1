e10f5c2a8dc3ceca781cc976595c065d0ffe0982
niki-gor <nic.gor@yandex.ru>
2023-02-10 23:40:49 +0300

    Make separate files

--- source/max.js
+++ source/max.js
@@ -1,34 +1,3 @@
 'use strict';
 
 const max = numbers => Math.max(...numbers);
-
-
-class ValueError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "ValueError";
-    }
-}
-
-/**
- * Apply string of attributes to object
- * @param {Object} obj - object
- * @param {string} attrs - string of attributes of the form '.attr1.attr2.attrN'. Any attr can be numeric too
- * @returns {Object} - object that can be interpreted as obj['attr1']['attr2']['attrN']
- */
-function get(obj, attrs) {
-    if (!attrs.startsWith('.')) {
-        throw new ValueError("attrs must start with '.'")
-    }
-    if (attrs == '.') {
-        return obj;
-    }
-    try {
-        return eval('obj' + attrs.replaceAll(/\.(\w+)/g, "['$1']"));
-    } catch (e) {
-        if (e.name != 'TypeError') { // TypeError appears in case 'undefined.attr' which is fine
-            throw e;
-        }
-    }
-    return undefined;
-}
