99ba15ca4b5e6fdbc60f8debb08ff991baf57a75
Daria Taktashova <taktashovadasha@yandex.ru>
2023-02-10 12:39:27 +0300

    added jsdoc and fixed tests

--- source/zip.js
+++ source/zip.js
@@ -1,3 +1,9 @@
 'use strict';
 
+/**
+ * Zip fields of given objects in one object.
+ * @param {...object} objects - An iterable containing a list of objects.
+ * @returns {object} A new object whose properties are given by the objects
+ * @author Taktashova Daria <taktashovadasha@yandex.ru>
+ */
 const zip = (...objects) => Object.assign({}, ...objects.reverse());
