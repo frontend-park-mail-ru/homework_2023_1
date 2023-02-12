4adf35374e40ceebb31d6b72aa46e09cc457b846
Nikita <rioskill@yandex.ru>
2023-02-12 19:30:10 +0300

    documentation

--- source/solve.js
+++ source/solve.js
@@ -1,26 +1,56 @@
+/**
+ * Solves a valid mathematical expression with regard to passed argument
+ * @param {string} expression consists of x, integers, basic operators and parantheses
+ * @param {number} argument value of x in expression
+ * @namespace solve
+ */
 const solve = (expression, argument) => {
+    "use strict"
 
+    /**
+     * Reverses the sequence of function arguments
+     * @type {function(...any): function(...any)}
+     * @param {function(...any)} f
+     */ 
     const flip_arguments = f => (...args) => f(...args.reverse())
 
-    const op = (priority, func) => {
-        return {priority: priority, exec: flip_arguments(func)};
-    };
+    /**
+     * An operator object
+     * @param {number} priority priority of operator
+     * @param {function(number, number)} func the callback invoked when operator is executed
+     */
+    const op = (priority, func) => ({priority: priority, exec: func});
 
     const ops = {
         "+": op(1, (a, b) => a + b),
         "-": op(1, (a, b) => a - b),
         "*": op(2, (a, b) => a * b),
-        "^": op(3, (a, b) => a ** b)
+        "^": op(3, (a, b) => Math.pow(a, b))
     };
 
+    /**
+     * Returns the top element of an array without popping it
+     * @param {Array} stack 
+     */
     const top = stack => stack[stack.length - 1];
-    const apply = (op_stack, num_stack) => num_stack.push(ops[op_stack.pop()].exec(num_stack.pop(), num_stack.pop()));
+
+    /**
+     * Takes the top operator from the op_stack and applies it to two numbers from num_stack
+     * As we pull numbers from the stack, they are in the reverse order; hence, flip_arguments is used
+     * @param {Array} op_stack 
+     * @param {Array} num_stack 
+     */
+    const apply = (op_stack, num_stack) => num_stack.push(flip_arguments(ops[op_stack.pop()].exec)(num_stack.pop(), num_stack.pop()));
+
     let op_stack = [];
     let num_stack = [];
 
     let parentheses_balance = 0;
 
-    for (symbol of expression) {
+    for (const symbol of expression) {
+        if (symbol === ' ')
+            continue;
+
         const num = parseInt(symbol);
 
         if (num) {
@@ -44,6 +74,8 @@ const solve = (expression, argument) => {
             }
             op_stack.pop();
             parentheses_balance--;
+        } else {
+            throw Error(`invalid symbol in expression: ${symbol}`);
         }
     }
 
