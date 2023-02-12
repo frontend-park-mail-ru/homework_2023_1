19064b47b73550c7f6610c4bf73f7e1f353ec062
Fedor <bazaleevf@gmail.com>
2023-02-12 15:58:18 +0300

    change return type JSDoc

--- source/chess.js
+++ source/chess.js
@@ -8,21 +8,21 @@ const ODD_LINE_PART = SPACE + STAR;
 
 /**
  * The function returns an even checkerboard row of stars and spaces.
- * @param {int} numbers - size of line.
+ * @param {number} numbers - size of line.
  * @returns {string} even row of chessboard of stars and spaces.
  */
 const getEvenLine = (numbers) => EVEN_LINE_PART.repeat(numbers / 2) + STAR.repeat(numbers % 2) + NEWLINE;
 
 /**
  * The function returns an odd checkerboard row of stars and spaces.
- * @param {int} numbers - size of line.
+ * @param {number} numbers - size of line.
  * @returns {string} odd row of chessboard of stars and spaces.
  */
 const getOddLine = (numbers) => ODD_LINE_PART.repeat(numbers / 2) + SPACE.repeat(numbers % 2) + NEWLINE;
 
 /**
  * A function that returns a chessboard N*N of stars and spaces.
- * @param {int} numbers - size of chessboard.
+ * @param {number} numbers - size of chessboard.
  * @returns {string | null} chessboard of stars and spaces.
  */
 const chess = (numbers) => {
