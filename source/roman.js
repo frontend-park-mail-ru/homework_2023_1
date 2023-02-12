c49a7ae5debf464c08281126e06aa6d1e4d28a0d
reddiridabl666 <reddiridabl666@gmail.com>
2023-02-12 14:24:22 +0300

    Code style fix

--- source/roman.js
+++ source/roman.js
@@ -27,63 +27,6 @@ const roman = (input) => {
         return NaN;
     }
 
-    /**
-     * Перевод чисел из римской записи в арабскую
-     * @param {string} str - Строка, содержащая римскую запись числа
-     * @returns {number} - Результат преобразования в арабскую запись
-     */
-    const fromRoman = (str) => {
-        const processedInput = str.toUpperCase().split('');
-
-        if (!processedInput.every((elem) => Object.keys(ARABIC).includes(elem))) {
-            return NaN;
-        }
-
-        return processedInput.reduce((res, cur, ind, arr) => {
-            const currentArabic = ARABIC[cur];
-
-            if (ind + 1 < arr.length && ARABIC[arr[ind + 1]] > currentArabic) {
-                return res - currentArabic;
-            }
-
-            return res + currentArabic;
-        }, 0);
-    }
-
-    /**
-     * Перевод чисел из арабской записи в римскую
-     * @param {number} number - Арабская запись числа
-     * @returns {string} - Результат преобразования в римскую запись
-     */
-    const toRoman = (number) => {
-        if (number > MAX_ROMAN || number < MIN_ROMAN)
-            return NaN;
-
-        return Object.keys(ROMAN).reverse().reduce((res, cur, ind, arr) => {
-            const digit = Math.floor(number / cur);
-
-            number %= cur;
-
-            if (digit === 4) {
-                return res + ROMAN[cur] + ROMAN_FIVE_MULTIPLES[cur];
-            }
-
-            if (digit === 9) {
-                return res + ROMAN[cur] + ROMAN[arr[ind - 1]];
-            }
-
-            if (digit >= 5) {
-                return res + ROMAN_FIVE_MULTIPLES[cur] + ROMAN[cur].repeat(digit % 5);
-            }
-
-            if (digit > 0) {
-                return res + ROMAN[cur].repeat(digit);
-            }
-
-            return res;
-        }, '');
-    }
-
     const numberInput = parseInt(input);
 
     if (Number.isNaN(numberInput)) {
@@ -91,4 +34,62 @@ const roman = (input) => {
     }
 
     return toRoman(numberInput);
-}
+};
+
+/**
+ * Перевод чисел из римской записи в арабскую
+ * @param {string} str - Строка, содержащая римскую запись числа
+ * @returns {number} - Результат преобразования в арабскую запись
+ */
+const fromRoman = (str) => {
+    const processedInput = str.toUpperCase().split('');
+
+    if (!processedInput.every(elem => Object.keys(ARABIC).includes(elem))) {
+        return NaN;
+    }
+
+    return processedInput.reduce((res, cur, ind, arr) => {
+        const currentArabic = ARABIC[cur];
+
+        if (ind + 1 < arr.length && ARABIC[arr[ind + 1]] > currentArabic) {
+            return res - currentArabic;
+        }
+
+        return res + currentArabic;
+    }, 0);
+};
+
+/**
+ * Перевод чисел из арабской записи в римскую
+ * @param {number} number - Арабская запись числа
+ * @returns {string} - Результат преобразования в римскую запись
+ */
+const toRoman = (number) => {
+    if (number > MAX_ROMAN || number < MIN_ROMAN) {
+        return NaN;
+    }
+
+    return Object.keys(ROMAN).reverse().reduce((res, cur, ind, arr) => {
+        const digit = Math.floor(number / cur);
+
+        number %= cur;
+
+        if (digit === 4) {
+            return res + ROMAN[cur] + ROMAN_FIVE_MULTIPLES[cur];
+        }
+
+        if (digit === 9) {
+            return res + ROMAN[cur] + ROMAN[arr[ind - 1]];
+        }
+
+        if (digit >= 5) {
+            return res + ROMAN_FIVE_MULTIPLES[cur] + ROMAN[cur].repeat(digit % 5);
+        }
+
+        if (digit > 0) {
+            return res + ROMAN[cur].repeat(digit);
+        }
+
+        return res;
+    }, '');
+};
