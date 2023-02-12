3ac18034c315713dacbc2f50882bb658f5a7af62
Aleksandr <sasha.oparin.1984@mail.ru>
2023-02-12 21:20:56 +0300

    Add jsDoc and check for null/undefined string values

--- source/minmax.js
+++ source/minmax.js
@@ -1,9 +1,25 @@
 'use strict';
 
+/**
+ * @description Функция для поиска минимального числа в массиве чисел
+ * @param {Array} numbers - массив чисел
+ * @returns {number} Минимальное число
+*/
 const min = numbers => Math.min(...numbers);
+
+/**
+ * @description Функция для поиска максимального числа в массиве чисел
+ * @param {Array} numbers - массив чисел
+ * @returns {number} Максимальное число
+*/
 const max = numbers => Math.max(...numbers);
 
-const minmax = (str) => {
-    let numbers = str.split(' ').filter(Boolean).map(Number).filter((value) => !Number.isNaN(value))
-    return numbers.length ? [min(numbers), max(numbers)] : [undefined, undefined]
+/**
+ * @description Функция для поиска минимального и максимального чисел в строке
+ * @param {string} str - строка с числа, разделёнными пробелами
+ * @returns {Array} Массив, содержащий минимальное и максимальное числа
+*/
+const minmax = str => {
+    const numbers = str ? str.split(' ').map(Number).filter((value) => !Number.isNaN(value)) : [];
+    return numbers.length ? [min(numbers), max(numbers)] : [undefined, undefined];
 };
