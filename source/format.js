80d73431430495fd6747564600f0cc1cc791341a
thxtomslf <75722306+thxtomslf@users.noreply.github.com>
2023-02-12 13:45:26 +0300

    Исправление замечаний

--- source/format.js
+++ source/format.js
@@ -1,14 +1,47 @@
 'use strict'
 
-
+/**
+ * @description Returns input values 'values', splitted into 'columnsNumber' columns
+ * columnsNumber > 0
+ * @param {Array.<Number>} values - Array of integers
+ * @param {Number} columnsNumber - Columns number
+ * @returns {String} String format table of input values with 'columnsNumber' columns 
+ */
 function format(values, columnsNumber) {
+	if (!Array.isArray(values)) {
+		throw new TypeError(
+			`Wrong type of first argument.
+			Expected "Array<Number>", got "${typeof values}"`
+		);
+	}
+
+	if (typeof columnsNumber !== 'number') {
+		throw new TypeError(
+			`Wrong type of second argument.
+			Expected "number", got "${typeof columnsNumber}"`
+		);
+	}
+
+	if (columnsNumber.length < 1) {
+		throw new TypeError(
+			`Wrong value of second argument. 'columnsNumber' must be > 0`
+		); 
+	}
+
 	let maxLengths = Array(columnsNumber).fill(0);
 
 	let columnIndex = 0;
 
-	// находим максимальные "длины" чисел по столбцам
-	for (let i = 0; i < values.length; i++) {
-		let newLength = getNumberLength(values[i]);
+	// получаем максимальные длины по столбцам
+	values.forEach((value) => {
+		if (typeof value != "number") {
+			throw new TypeError(
+				`Wrong type of array values.
+				Expected "Number", got "${typeof value}"`
+			);
+		}
+
+		let newLength = getNumberLength(value);
 		if (newLength > maxLengths[columnIndex]) {
 			maxLengths[columnIndex] = newLength;
 		}
@@ -17,16 +50,16 @@ function format(values, columnsNumber) {
 		if (columnIndex >= columnsNumber) {
 			columnIndex = 0;
 		}
-	}
+	});
 
 	// формируем строку
 	columnIndex = 0;
 	let output = "";
-	for (let i = 0; i < values.length; i++) {
-		output += formatNumber(values[i], maxLengths[columnIndex]);
+	values.forEach((value, index, values) => {
+		output += formatNumber(value, maxLengths[columnIndex]);
 
-		if (i == values.length - 1) {
-			break;
+		if (index == values.length - 1) {
+			return;
 		}
 
 		if (columnIndex >= columnsNumber - 1) {
@@ -36,13 +69,18 @@ function format(values, columnsNumber) {
 			output += ' ';
 			columnIndex++;
 		}
-	}
+	});
 
 	return output;
 
 }
 
-// функция приводит число к виду ячейки таблицы
+/**
+ * @description Creates table cell out of input number (adds " ")
+ * @param {Number} number - Input number 
+ * @param {Number} length - Table cell length
+ * @returns {String} Table cell 
+ */
 function formatNumber(number, length) {
 	let stringedNumber = number.toString();
 
@@ -55,10 +93,13 @@ function formatNumber(number, length) {
 }
 
 
-// функция возращает длину числа
+/**
+ * @description Returns number length
+ * @param {Number} number - Input number 
+ * @returns {Number} Number 'length'
+ */
 function getNumberLength(number) {
 	let length = number < 0 ? 2 : 1;
-
 	while (~~(number / 10) != 0) {
 		number /= 10;
 		length++;
