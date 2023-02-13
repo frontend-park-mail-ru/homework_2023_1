97613f6309382efaace086b0843c836ab172546f
Daria Taktashova <taktashovadasha@yandex.ru>
2023-02-13 18:27:26 +0300

    final commit#2

--- source/zip.js
+++ source/zip.js
@@ -6,4 +6,9 @@
  * @returns {object} A new object whose properties are given by the objects
  * @author Taktashova Daria <taktashovadasha@yandex.ru>
  */
-const zip = (...objects) => objects.reduceRight((acc, cur) => ({ ...acc, ...cur}), {});
+const zip = (...objects) => {
+    if (objects != null && objects.every(elem => elem != null)) {
+        return objects.reduceRight((acc, cur) => ({ ...acc, ...cur}), {});
+    }
+    throw new Error('One of elements is null or undefined')
+}
