45b8bc01e98b6813a6132015313528f7e876df07
Timofey Chichikin <72801901+ned8800@users.noreply.github.com>
2023-02-13 18:48:47 +0300

    divided error messages and used inner JS functions

--- source/inverse.js
+++ source/inverse.js
@@ -6,29 +6,23 @@
 // месте остаются элементы в конце массива
 
 
+
 const inverse = (given_array, position = 0) => {
-    if ((!Array.isArray(given_array)) || (!Number.isInteger(position))) {
-        throw new Error("wrong parameters!")
+    if (!Array.isArray(given_array)) {
+        throw new Error("First parameter is not an array!")
     }
 
-    const temp_given_array = [...given_array]
+    if (!Number.isInteger(position)) {
+        throw new Error("Second parameter is not integer type!")
+    }
+
+    let temp_given_array = [...given_array]
 
     if (position < 0) {
-        for (let i = given_array.length + position - 1; i >= 0; i--) {
-            temp_given_array[i] = given_array[given_array.length - 1 - i + position]
+        temp_given_array = given_array.slice(0, position).reverse().concat(given_array.slice(position));
 
-
-        }
     } else  if (position >= 0) {
-        for (let i = given_array.length - 1; i >= position; i--) {
-            temp_given_array[i] = given_array[given_array.length - 1 - i + position]
-        }
+        temp_given_array = given_array.slice(0, position).concat(given_array.slice(position).reverse());
     }
-
     return temp_given_array
 };
-
-
-
-
-
---
+++ source/inverse_old.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Напишите функцию inverse, которая меняет порядок элементов в массике на противоположный.
+// Если в функцию вторым аргументом передаётся число — то переставляются все элементы массива
+// кроме нескольких первых (количество зависит от числа). Если число отрицательное — то на
+// месте остаются элементы в конце массива
+
+
+
+// old realisation
+// const inverse = (given_array, position = 0) => {
+//     if ((!Array.isArray(given_array)) || (!Number.isInteger(position))) {
+//         throw new Error("wrong parameters!")
+//     }
+
+//     const temp_given_array = [...given_array]
+
+//     if (position < 0) {
+//         for (let i = given_array.length + position - 1; i >= 0; i--) {
+//             temp_given_array[i] = given_array[given_array.length - 1 - i + position]
+
+
+//         }
+//     } else  if (position >= 0) {
+//         for (let i = given_array.length - 1; i >= position; i--) {
+//             temp_given_array[i] = given_array[given_array.length - 1 - i + position]
+//         }
+//     }
+
+//     return temp_given_array
+// };
+
+
+
+
+
