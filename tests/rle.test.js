b6bfb504c9dc69d502f4e05f3373d4519b3eb93e
moraPresence <od34156la@mail.ru>
2023-02-13 10:58:13 +0300

    test done

--- tests/rle.test.js
+++ tests/rle.test.js
@@ -1,46 +1,49 @@
 'use strict';
 
-QUnit.module('Тестируем функцию rle', function () {
-  QUnit.test('rle отрабатывает верно на строке с одной заменой в начале', function (assert) {
+QUnit.module('Тестируем функцию rle: success', function () {
+  QUnit.test('rle с одной заменой в начале', function (assert) {
     assert.strictEqual(rle('AAAB'), 'A3B', 'rle(AAAB) === A3B');
   });
 
-  QUnit.test('rle отрабатывает верно на строке с одной заменой в конце', function (assert) {
+  QUnit.test('rle  с одной заменой в конце', function (assert) {
     assert.strictEqual(rle('ABBB'), 'AB3', 'rle(ABBB) === AB3');
   });
 
-  QUnit.test('rle отрабатывает верно на строке с множественной заменой', function (assert) {
+  QUnit.test('rle с множественной заменой', function (assert) {
     assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
     assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'),
       'AV3B3V2XDHJF4D6HA4J3D2SLS3D4',
       'rle(AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD) === AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
   });
 
-  QUnit.test('rle отрабатывает верно на строке состоящей из цифр', function (assert) {
+  QUnit.test('rle на строке состоящей из одного символа', function (assert) {
+    assert.strictEqual(rle('a'), 'a', 'rle(a) === a');
+  });
+
+  QUnit.test('rle на строке состоящей из повторений одного символа', function (assert) {
+    assert.strictEqual(rle('aaaaaa'), 'a6', 'rle(aaaaaa) === a6');
+  });
+
+  QUnit.test('rle на пустой строке', function (assert) {
+    assert.strictEqual(rle(''), '', 'rle() === ');
+  });
+});
+
+QUnit.module('Тестируем функцию rle: bad input', function () {
+
+  QUnit.test('rle на строке состоящей из цифр', function (assert) {
     assert.throws(function () {
       rle('33334')
     }, Error("there are numbers in the string"), 'rle(33334) === Error("there are numbers in the string")');
   });
 
-  QUnit.test('rle отрабатывает верно на строке состоящей из одного символа', function (assert) {
-    assert.strictEqual(rle('a'), 'a', 'rle(a) === a');
-  });
-
-  QUnit.test('rle отрабатывает верно на строке состоящей из повторений одного символа', function (assert) {
-    assert.strictEqual(rle('aaaaaa'), 'a6', 'rle(aaaaaa) === a6');
-  });
-
-  QUnit.test('rle отрабатывает верно на пустой строке', function (assert) {
-    assert.strictEqual(rle(''), '', 'rle() === ');
-  });
-
-  QUnit.test('rle отрабатывает верно без аргументов', function (assert) {
+  QUnit.test('rle без аргументов', function (assert) {
     assert.throws(function () {
       rle()
     }, Error("wrong input"), 'rle() === Error("wrong input")');
   });
 
-  QUnit.test('rle отрабатывает верно на неправильных аргументах', function (assert) {
+  QUnit.test('rle на неправильных аргументах', function (assert) {
     assert.throws(function () {
       rle(123)
     }, Error("wrong input"), 'rle(123) === Error("wrong input")');
