1c3758cf33f7623685f101e090f0f7f655ded8d9
Ivan Bulgakov <wanjasoldier@gmail.com>
2023-02-14 02:46:13 +0300

    enclosed arguments of plain in brackets

--- source/plain.js
+++ source/plain.js
@@ -5,7 +5,7 @@
  * @param {Array} arr - original array
  * @returns {Array} flattened arr
 */
-const plain = arr => {
+const plain = (arr) => {
     if (!Array.isArray(arr)) {
         throw new TypeError('arr must be an array!');
     }
