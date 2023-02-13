6ae9bccdadff91552fa01c877b4782b53871fff2
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-02-13 21:17:21 +0300

    1) Написано без использования метода .reverse 2) Написаны тесты на функцию 3) Сделана проверка на валидацию входящих данных для функции 4) Написана документация

--- source/inverse.js
+++ source/inverse.js
@@ -1,14 +1,30 @@
 'use strict';
-
-// Напишите функцию `inverse`, которая меняет порядок элементов в массике на противоположный.
-// Если в функцию вторым аргументом передаётся число &mdash; то переставляются все элементы массива кроме нескольких первых (количество зависит от числа).
-// Если число отрицательное &mdash; то на месте остаются элементы в конце массива
-
-const inverse = (array, number) => {
-    if (number > 0) {
-        return array.slice(0, number).concat(array.slice(number).reverse());
-    } else if (number < 0) {
-        return array.slice(0, number).reverse().concat(array.slice(number));
+/**
+ * Inverse array except for elements that are defined by param stop
+ * @function inverse 
+ * @param {Array} array - array of elements
+ * @param {*} pos - position of element from which the array will be reversed
+ * @returns {Array}
+ */
+const inverse = (array, pos = 0) => {
+    if (!Array.isArray(array)) {
+        throw new Error('First argument must be an array');
     }
-    return array.reverse();
+
+    if (typeof pos !== 'number') {
+        throw new Error('Second argument must be a number');
+    }
+
+    const newArray = [...array];
+    
+    if (pos < 0) {
+        for (let i = 0; i <= array.length + pos - 1; i++) {
+            newArray[i] = array[array.length + pos - i - 1];
+        }
+    } else {
+        for (let i = array.length - 1; i >= pos; i--) {
+            newArray[i] = array[array.length + pos - i - 1];
+        }
+    }
+    return newArray;
 }
--- tests/inverse.js
+++ tests/inverse.js
@@ -33,4 +33,66 @@ QUnit.module('Тестируем функцию inverse', function () {
 		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], -5), [ 1, 2, 3, 4, 5 ]);
 		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], -15), [ 1, 2, 3, 4, 5 ]);
 	});
+
+	QUnit.test('Функция работает с массивом из одинаковых элементов', function (assert) {
+		assert.deepEqual(inverse([ 1, 1, 1, 1, 1 ]), [ 1, 1, 1, 1, 1 ]);
+		assert.deepEqual(inverse([ 'a', 'a', 'a', 'a', 'a' ]), [ 'a', 'a', 'a', 'a', 'a' ]);
+		assert.deepEqual(inverse([ null, null, null, null, null ]), [ null, null, null, null, null ]);
+		assert.deepEqual(inverse([ false, false, false, false, false ]), [ false, false, false, false, false ]);
+	});
+
+	QUnit.test('Функция работает с массивом из одинаковых элементов и смещением', function (assert) {
+		assert.deepEqual(inverse([ 1, 1, 1, 1, 1 ], 1), [ 1, 1, 1, 1, 1 ]);
+		assert.deepEqual(inverse([ 'a', 'a', 'a', 'a', 'a' ], 1), [ 'a', 'a', 'a', 'a', 'a' ]);
+		assert.deepEqual(inverse([ null, null, null, null, null ], 1), [ null, null, null, null, null ]);
+		assert.deepEqual(inverse([ false, false, false, false, false ], 1), [ false, false, false, false, false ]);
+	});
+
+	QUnit.test('Функция работает с массивом из разных элементов', function (assert) {
+		assert.deepEqual(inverse([ 1, 'a', null, false ]), [ false, null, 'a', 1 ]);
+		assert.deepEqual(inverse([ 1, 'a', null, false, 0, Infinity, '' ]), [ '', Infinity, 0, false, null, 'a', 1 ]);
+	});
+
+	QUnit.test('Функция работает с пустым массивом', function (assert) {
+		assert.deepEqual(inverse([]), []);
+	});
+
+	QUnit.test('Функция работает с пустым массивом и смещением', function (assert) {
+		assert.deepEqual(inverse([], 1), []);
+		assert.deepEqual(inverse([], 2), []);
+		assert.deepEqual(inverse([], 3), []);
+	});
+
+	QUnit.test('Функция на ошибочные входные данные', function (assert) {
+		assert.throws(function () {
+			inverse();
+		}, 'Функция не принимает пустой массив');
+		assert.throws(function () {
+			inverse(1);
+		}, 'Функция не принимает число');
+		assert.throws(function () {
+			inverse('1');
+		}, 'Функция не принимает строку');
+		assert.throws(function () {
+			inverse(null);
+		}, 'Функция не принимает null');
+		assert.throws(function () {
+			inverse(undefined);
+		}, 'Функция не принимает undefined');
+		assert.throws(function () {
+			inverse({});
+		}, 'Функция не принимает объект');
+		assert.throws(function () {
+			inverse([ 1, 2, 3, 4, 5 ], '1');
+		}, 'Функция не принимает строку в качестве смещения');
+		assert.throws(function () {
+			inverse([ 1, 2, 3, 4, 5 ], null);
+		}, 'Функция не принимает null в качестве смещения');
+		assert.throws(function () {
+			inverse([ 1, 2, 3, 4, 5 ], {});
+		}, 'Функция не принимает объект в качестве смещения');
+		assert.throws(function () {
+			inverse([ 1, 2, 3, 4, 5 ], []);
+		}, 'Функция не принимает пустой массив в качестве смещения');
+	});
 });
