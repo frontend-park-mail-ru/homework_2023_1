8510c95cf34128681c7fc2e244db1a1f9d316136
niki-gor <nic.gor@yandex.ru>
2023-02-13 21:37:18 +0300

    Refactor code

--- source/get.js
+++ source/get.js
@@ -1,19 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Apply string of attributes to object
  * @param {Object} object
  * @param {string} attrs - string of attributes of the form '.attr1.attr2.attrN'. Any attr can be numeric too
- * @returns {Object|undefined} - object that can be interpreted as obj['attr1']['attr2']['attrN']
+ * @returns {*} - object that can be interpreted as obj['attr1']['attr2']['attrN']
  */
-function get(object, attrs) {
-    if (!attrs.startsWith('.')) {
-        throw new Error("attrs must start with '.'")
-    }
-    if (attrs === '.') {
-        return object
-    }
-    return attrs.slice(1)
-                .split('.')
-                .reduce((obj, attr) => obj === undefined ? obj : obj[attr], object);
-}
+const get = (object, attrs) => {
+  if (!attrs.startsWith(".")) {
+    throw new Error("attrs must start with '.'");
+  }
+  if (attrs === ".") {
+    return object;
+  }
+  return attrs
+    .slice(1)
+    .split(".")
+    .reduce((obj, attr) => (obj === undefined ? undefined : obj[attr]), object);
+};
