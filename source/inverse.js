8af5c4dfdd42e5080239fa618e3b8492a3e99fe1
Timofey Chichikin <72801901+ned8800@users.noreply.github.com>
2023-02-13 23:21:00 +0300

    Tests now lined properly

--- source/inverse.js
+++ source/inverse.js
@@ -1,3 +1,4 @@
+
 /**
  * Inverse array except the elements, which are divided by param position
  * @function inverse
@@ -8,13 +9,13 @@
  * @author Chichikin Timofey <tchichikin@mail.ru>
  */
 
-function inverse (given_array, position = 0) {
+const inverse = (given_array, position = 0) => { // можно создать стрелочно
     if (!Array.isArray(given_array)) {
-        throw new Error("First parameter is not an array!")
+        throw new TypeError('First parameter is not an array!')
     }
 
     if (!Number.isInteger(position)) {
-        throw new Error("Second parameter is not integer type!")
+        throw new TypeError('Second parameter is not integer type!')
     }
 
     let temp_given_array = [...given_array]
