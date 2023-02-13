dccd75c9f8570bc5ee7e980593e327c8f29649c8
Staurran <staurran@gmail.com>
2023-02-13 18:50:33 +0300

    added ";"

--- source/inverse.js
+++ source/inverse.js
@@ -9,24 +9,24 @@
  */
 const inverse = (numbers, stop = 0) => {
     if (!Array.isArray(numbers)) {
-        throw new Error("The first parameter is not array")
+        throw new Error("The first parameter is not array");
     }
 
     if (!Number.isInteger(stop)) {
-        throw new Error("The second parameter is not integer")
+        throw new Error("The second parameter is not integer");
     }
 
-    const newNumbers = [...numbers]
+    const newNumbers = [...numbers];
 
     if (stop >= 0) {
         for (let i = numbers.length - 1; i >= stop; i--) {
-            newNumbers[i] = numbers[numbers.length - 1 - i +stop]
+            newNumbers[i] = numbers[numbers.length - 1 - i +stop];
         }
     } else {
         for (let i = 0; i <= numbers.length + stop - 1; i++) {
-            newNumbers[i] = numbers[numbers.length - 1 - i + stop]
+            newNumbers[i] = numbers[numbers.length - 1 - i + stop];
         }
     }
 
-    return newNumbers
+    return newNumbers;
 };
