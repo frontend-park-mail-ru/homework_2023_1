41c408b481b1871139c2cab1c8e5ce36fe725d0b
IvanStukalov <ivanstukalov2003@gmail.com>
2023-02-13 15:31:52 +0300

    corrected jsdoc

--- source/sorting.js
+++ source/sorting.js
@@ -1,21 +1,20 @@
 'use strict';
 
 /**
- * Sorting an array
- * @sorting
- * @param {Array} arr - Array you want to sort
- * @param {Array} params - array of parameters you will sort on
+ * Sorting an array of objects
+ * @param {Array} arr - Array of objects you want to sort
+ * @param {Array} params - array of parameters as strings you will sort on
  * 
- * @return {Array} Array that have been sorted
+ * @return {Array} Array of objects that have been sorted
  * 
  * @author Ivan Stuaklov <sid21u699@student.bmstu.ru>
  */
-const sorting = function (arr, params) {
-    for (let curParam = 0; curParam < params.length; ++curParam) {
+function sorting (arr, params) {
+    params.forEach(element =>
         arr.sort((a, b) => {
             let wasCoincident = true;
-            for (let lastParam = 0; lastParam < curParam; ++lastParam){ // проверяем, равны ли значения по всем предыдущим параметрам
-                if (a[params[lastParam]] != b[params[lastParam]]) {     
+            for (let lastParam = 0; lastParam < element; ++lastParam){ // проверяем, равны ли значения по всем предыдущим параметрам
+                if (a[params[lastParam]] !== b[params[lastParam]]) {     
                     wasCoincident = false;
                 }
             }
@@ -23,14 +22,14 @@ const sorting = function (arr, params) {
                 if (a[params[curParam]] > b[params[curParam]]) {
                     return 1;
                 }
-                if (a[params[curParam]] == b[params[curParam]]) {
+                if (a[params[curParam]] === b[params[curParam]]) {
                     return 0;
                 }
                 if (a[params[curParam]] < b[params[curParam]]) {
                     return -1;
                 }
             }
-        });
-    }   
+        })
+    );   
     return arr;
 }
