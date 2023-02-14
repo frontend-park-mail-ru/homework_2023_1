ef563237677e5bbf838854fd04a897a496f0408a
CyTb-On-Fire <cytb2024@gmail.com>
2023-02-15 00:34:51 +0300

    fixed second iteration of requsted changes

--- source/euclid.js
+++ source/euclid.js
@@ -2,35 +2,25 @@
 
 /**
  * Нахождение наибольшего общего делителя натуральных чисел.
- * @param  nums - Массив натуральных чисел
+ * @param  nums {number} - Массив натуральных чисел
  * @returns {null|number} - Результат вычисления (натуральное число, если входные параметры корректны, иначе null)
  */
-function euclid(...nums) {
-    //проверка натуральных чисел
-    if (nums.some(number => !Number.isInteger(number) || number <= 0)){
+const euclid = function(...nums) {
+    if (nums.length === 0) {
         return null;
     }
-    //проверка единственного аргумента
-    if (nums.length === 1) {
-        return nums.at(0);
+    if (nums.some(number => !Number.isInteger(number) || number <= 0)) {
+        return null;
     }
-    /**
-     * Вычисление наибольшего общего делителя для двух натуральных чисел
-     * @param first - первое натуральное число
-     * @param second - второе натуральное число
-     * @returns {number} - результат вычисления
-     */
-    const CountGCD = function(first, second){
-        let largest = Math.max(first, second);
-        let lowest = Math.min(first, second);
-        let temp;
-        while(lowest !== 0){
-            temp = lowest;
+    return nums.reduce((pair_GCD, currentValue) => {
+        let largest = Math.max(pair_GCD, currentValue);
+        let lowest = Math.min(pair_GCD, currentValue);
+        while (lowest !== 0) {
+            const temp = lowest;
             lowest = largest % lowest;
             largest = temp;
         }
         return largest;
-    };
-    return nums.reduce((pair_GCD, currentValue) => CountGCD(pair_GCD, currentValue));
+    });
 }
 
