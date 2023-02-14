3c683a4e49cf7d98ffd73e29c0e501c76d4303a6
Dimitriev Evgeniy <dimitrievevgeniy@mail.ru>
2023-02-14 21:02:28 +0300

    fixes

--- source/tree.js
+++ source/tree.js
@@ -1,32 +1,49 @@
 'use strict';
+/**
+ * function creating ascii tree
+ * @returns {String | null}
+ * @example
+ * tree(3) return
+ *
+ * '  *  \n'
+ * ' *** \n'
+ * '*****\n'
+ * '  |  \n'
+ * @param n - height
+ */
 
-
-function* print_symbol(count,symbol){
-    for(let  i = 0;i<count;++i) yield symbol;
+/** This function is returning a symbol. */
+function* print_symbol(count, symbol) {
+    for (let i = 0; i < count; ++i) yield symbol;
 }
-function* row(height,cur_height,symbol){
-    yield* print_symbol(height - 1 - cur_height,' ')
 
-    yield* print_symbol(2 * cur_height - 1,symbol)
+/** This function is generating a symbol. */
+function* row(height, cur_height, symbol) {
+    yield* print_symbol(height - 1 - cur_height, ' ')
 
-    yield* print_symbol(height - 1 - cur_height,' ')
+    yield* print_symbol(2 * cur_height - 1, symbol)
+
+    yield* print_symbol(height - 1 - cur_height, ' ')
 
     yield "\n";
 }
 
+/** This function is generating a tree. */
 function* create_tree(height) {
-    for(let i = 1; i < height; i++)
+    for (let i = 1; i < height; i++)
         yield* row(height, i, '*');
 
     yield* row(height, 1, '|');
 }
-const tree = (n) =>{
+
+
+const tree = (n) => {
     const height = Number(n);
-    if((isNaN(height))||n===null||(!Number.isInteger(height))||(!isFinite(height))){
+    if ((isNaN(height)) || n === null || (!Number.isInteger(height)) || (!isFinite(height))) {
         throw new TypeError('такой высоты не бываает');
     }
 
-    if(height<3){
+    if (height < 3) {
         return null;
     }
 
