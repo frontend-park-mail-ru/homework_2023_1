5f396349db4866aa9b25755aa728f85233f5878c
stepanpopov <stepan@DESKTOP-81FUGHF>
2023-02-14 17:16:41 +0300

    changes from review + search logic improved

--- source/filter.js
+++ source/filter.js
@@ -1,12 +1,14 @@
 'use strict';
 
-let extra = new Map([['>', "&gt;"],
-                    ['<', "&lt;"],
-                    ['"', "&quot;"],
-                    ['&', "&amp;"],
-                    ['\'', "&#39;"]]);
+const extra = { 
+    '>' : '&gt;',
+    '<' : '&lt;',
+    '"' : '&quot;',
+    '&' : '&amp;',
+    '\'' : '&#39;' 
+};
 
-/**
+/*
  * Filter html block
  * @param {string} - Input html block
  * @param {Object} array of strings - Tags to leave as they are
@@ -14,38 +16,46 @@ let extra = new Map([['>', "&gt;"],
  * 
  * @author Popov Stepan
  */
-let filter = function(input, tags) {
-    let output = ""
-    for (let i = 0; i < input.length; ++i) {
-        let changedSymb = extra.get(input[i])
+let filter = (input, tags) => {
+    let output = '';
+    input.split('').forEach((symb) => {
+        let changedSymb = extra[symb];
         if (changedSymb === undefined) {
-            output += input[i]
+            output += symb;
         } else {
-            output += changedSymb
+            output += changedSymb;
         }
-    }
+    });
     
     if (tags === undefined) {
-        return output
+        return output;
     }
     
-    let gt_sym_len = extra.get('>').length;
-    let lt_sym_len = extra.get('<').length;
-    for (let tag of tags) {
+    let gt_sym = extra['>'];
+    let lt_sym = extra['<'];
+    let gt_sym_len = gt_sym.length;
+    let lt_sym_len = lt_sym.length;
+    tags.forEach(tag => {
         let tag_len = tag.length; 
         
-        let pos = -1;
+        let pos = -1; 
         while ((pos = output.indexOf(tag, pos + 1)) != -1) {
-            if (output[pos - 1] == '/') {
-                output = output.slice(0, pos - lt_sym_len - 1) + 
-                         "</" + tag + ">" + 
-                         output.slice(pos + tag_len + gt_sym_len);
-            } else {
-                output = output.slice(0, pos - lt_sym_len) + 
-                         "<" + tag + ">" + 
-                         output.slice(pos + tag_len + gt_sym_len);
+            if (output.slice(pos - lt_sym_len, pos) !== lt_sym && 
+                output.slice(pos - lt_sym_len - 1, pos) !== (lt_sym + '/')) {
+                continue;
             }
+
+            let left_part = (output[pos - 1] === '/') 
+                ? (output.slice(0, pos - lt_sym_len - 1)) 
+                : (output.slice(0, pos - lt_sym_len));
+            let gt_sym_pos = output.indexOf(gt_sym, pos);
+            let tag_part = (output[pos - 1] === '/')
+                ? ('</' + tag + '>')
+                : ('<' + tag + output.slice(pos + tag_len, gt_sym_pos) + '>');
+            let right_part = output.slice(gt_sym_pos + gt_sym_len);
+
+            output = left_part + tag_part + right_part;
         }
-    }
+    });
     return output
 };
--- tests/filter.js
+++ tests/filter.js
@@ -27,12 +27,49 @@ QUnit.module('Проверка работы функции filter', function () 
 	});
 
 	QUnit.test('filter проверяет несколько вложенных тегов', function (assert) {
-		const input = "<ul> <li> <a>alert('1');</a> </li> </ul>"
+		const input = `<ul> <li> <a>alert('1');</a> </li> </ul>`;
 
-		const output = filter(input, ["ul", "a"])
+		const output = filter(input, ['ul', 'a']);
 
-		const expected = "<ul> &lt;li&gt; <a>alert(&#39;1&#39;);</a> &lt;/li&gt; </ul>";
+		const expected = `<ul> &lt;li&gt; <a>alert(&#39;1&#39;);</a> &lt;/li&gt; </ul>`;
 
 		assert.strictEqual(output, expected);
+	});
+
+	QUnit.test('filter проверяет теги, которые имеют вложенные атрибуты', function (assert) {
+		const input = `<a class="navbar-brand" href="#">Technochan </a> <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"> <span class="navbar-toggler-icon"></span> </button>`;
+		const output = filter(input, ['a', 'button']);
+
+		const expected = `<a class=&quot;navbar-brand&quot; href=&quot;#&quot;>Technochan </a> <button class=&quot;navbar-toggler&quot; data-bs-toggle=&quot;collapse&quot; data-bs-target=&quot;#navbarCollapse&quot;> &lt;span class=&quot;navbar-toggler-icon&quot;&gt;&lt;/span&gt; </button>`;
+
+		assert.strictEqual(output, expected);
+	});
+
+	QUnit.test('filter проверяет один тег без пробелов', function (assert) {
+		const input1 = `<div></div>`;
+		const output1 = filter(input1);
+		const expected1 = `&lt;div&gt;&lt;/div&gt;`;
+
+		assert.strictEqual(output1, expected1);
+
+		const input2 = `<div></div>`;
+		const output2 = filter(input2, ['div']);
+		const expected2 = `<div></div>`;
+
+		assert.strictEqual(output2, expected2);
+	});
+
+	QUnit.test('filter: ситуация, когда тег содержит аттрибут, имя которого совпадает с тегом', function (assert) {
+		const input1 = `<button type = button> click() </button>`;
+		const output1 = filter(input1);
+		const expected1 = `&lt;button type = button&gt; click() &lt;/button&gt;`;
+
+		assert.strictEqual(output1, expected1);
+
+		const input2 = `<button type=button> click() </button>`;
+		const output2 = filter(input2, ['button']);
+		const expected2 = `<button type=button> click() </button>`;
+
+		assert.strictEqual(output2, expected2);
 	});
 });
