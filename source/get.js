12ced5aa3b48f4f74717f6e5e16bbf972f098ba8
niki-gor <nic.gor@yandex.ru>
2023-02-14 17:25:38 +0300

    Replace 4-spaced-tab with real tab

--- source/get.js
+++ source/get.js
@@ -7,20 +7,20 @@
  * @returns {*} - object that can be interpreted as obj['attr1']['attr2']['attrN']
  */
 const get = (object, attrs) => {
-    // обращение к строке тоже валидно (можно обратиться по индексу)
-    if (typeof object !== 'object' && typeof object !== 'string') {
-        throw new TypeError("typeof object (param) should be 'object' or 'string'");
-    }
-    if (typeof attrs !== 'string') {
-        throw new TypeError('attrs should be string');
-    }
-    if (!attrs.startsWith('.')) {
-        throw new Error("attrs must start with '.'");
-    }
-    if (attrs === '.') {
-        return object;
-    }
-    return attrs.slice(1)
-                .split('.')
-                .reduce((obj, attr) => obj === undefined ? undefined : obj[attr], object);
+	// обращение к строке тоже валидно (можно обратиться по индексу)
+	if (typeof object !== 'object' && typeof object !== 'string') {
+		throw new TypeError("typeof object (param) should be 'object' or 'string'");
+	}
+	if (typeof attrs !== 'string') {
+		throw new TypeError('attrs should be string');
+	}
+	if (!attrs.startsWith('.')) {
+		throw new Error("attrs must start with '.'");
+	}
+	if (attrs === '.') {
+		return object;
+	}
+	return attrs.slice(1)
+				.split('.')
+				.reduce((obj, attr) => obj === undefined ? undefined : obj[attr], object);
 };
