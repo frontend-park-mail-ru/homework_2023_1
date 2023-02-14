fedf45a8f70cb6fe51fda8b48ad0468f074ecf2c
IvanStukalov <ivanstukalov2003@gmail.com>
2023-02-14 20:06:38 +0300

    added types for jsdoc

--- source/sorting.js
+++ source/sorting.js
@@ -2,10 +2,10 @@
 
 /**
  * Sorting an array of objects
- * @param {Array} arr - Array of objects you want to sort
- * @param {Array} params - array of parameters as strings you will sort on
+ * @param {Array<Object.<string, *>>} arr - Array of objects you want to sort
+ * @param {Array<string>} params - array of parameters as strings you will sort on
  * 
- * @return {Array} Array of objects that have been sorted
+ * @return {Array<Object.<string, *>>} Array of objects that have been sorted
  * 
  * @author Ivan Stuaklov <sid21u699@student.bmstu.ru>
  */
