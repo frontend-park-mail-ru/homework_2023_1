ea336965d0d1a8f87cc91e8fb155d822a8aa7d6e
stepanpopov <stepan@DESKTOP-81FUGHF>
2023-02-16 12:28:17 +0300

    fix

--- source/filter.js
+++ source/filter.js
@@ -16,45 +16,34 @@ const extra = {
  * 
  * @author Popov Stepan
  */
-let filter = (input, tags) => {
+const filter = (input, tags) => {
     let output = '';
     input.split('').forEach((symb) => {
-        let changedSymb = extra[symb];
-        if (changedSymb === undefined) {
-            output += symb;
-        } else {
-            output += changedSymb;
-        }
+        output += extra[symb] || symb
     });
     
-    if (tags === undefined) {
+    if (!tags) {
         return output;
     }
     
-    let gt_sym = extra['>'];
-    let lt_sym = extra['<'];
-    let gt_sym_len = gt_sym.length;
-    let lt_sym_len = lt_sym.length;
+    const gt_sym = extra['>'];
+    const lt_sym = extra['<'];
+    const gt_sym_len = gt_sym.length;
+    const lt_sym_len = lt_sym.length;
     tags.forEach(tag => {
-        let tag_len = tag.length; 
+        const tag_len = tag.length; 
         
         let pos = -1; 
-        while ((pos = output.indexOf(tag, pos + 1)) != -1) {
+        while ((pos = output.indexOf(tag, pos + 1)) !== -1) {
             if (output.slice(pos - lt_sym_len, pos) !== lt_sym && 
-                output.slice(pos - lt_sym_len - 1, pos) !== (lt_sym + '/')) {
+                output.slice(pos - lt_sym_len - 1, pos) !== (`${lt_sym}/`)) {
                 continue;
             }
 
-            let left_part = (output[pos - 1] === '/') 
-                ? (output.slice(0, pos - lt_sym_len - 1)) 
-                : (output.slice(0, pos - lt_sym_len));
-            let gt_sym_pos = output.indexOf(gt_sym, pos);
-            let tag_part = (output[pos - 1] === '/')
-                ? ('</' + tag + '>')
-                : ('<' + tag + output.slice(pos + tag_len, gt_sym_pos) + '>');
-            let right_part = output.slice(gt_sym_pos + gt_sym_len);
-
-            output = left_part + tag_part + right_part;
+            const gt_sym_pos = output.indexOf(gt_sym, pos);
+            output = (output[pos - 1] === '/')
+                ? `${output.slice(0, pos - lt_sym_len - 1)}</${tag}>${output.slice(gt_sym_pos + gt_sym_len)}`
+                : `${output.slice(0, pos - lt_sym_len)}<${tag}${output.slice(pos + tag_len, gt_sym_pos)}>${output.slice(gt_sym_pos + gt_sym_len)}`
         }
     });
     return output
